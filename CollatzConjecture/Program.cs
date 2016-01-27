b6453733310a9f8e7c0124ec6f0f97ddee8ad4f5
Conrad Creel <conrad.creel@mindbodyonline.com>
2016-01-27 15:31:56 -0800

    Modified to solve the problem here: http://www.programming-challenges.com/pg.php?page=downloadproblem&probid=110101&format=html

--- CollatzConjecture/Program.cs
+++ CollatzConjecture/Program.cs
@@ -10,57 +10,69 @@ namespace CollatzConjecture
     {
         const int MAX_EXPONENT = 24;
         const int MILLION = 1000000;
+        //const int MAX_ITERATIONS = 10000000; // Try 10 million iterations and then stop.
+
         static Dictionary<int, int> _CollatzLookup;
 
         static void Main(string[] args)
         {
             InitializeLookup();
 
-            int num = 22;
-            int tmpNum = num;
-
-            if (CheckNumber(num))
+            int i = 900, j = 1000;
+            var cycleLengths = new List<int>();
+            for (int num = i; num <= j; num++)
             {
-                var sequence = new List<int>();
-                int cycleLength = 0;
-                bool calculated = false;
-
-                sequence.Add(tmpNum);
-                do
+                if (CheckNumber(num))
                 {
-                    tmpNum = f(tmpNum);
-                    sequence.Add(tmpNum);
-                    if (tmpNum == 1)
-                    {
-                        calculated = true;
-                    }
-                    else if (_CollatzLookup.ContainsKey(tmpNum))
-                    {
-                        cycleLength = sequence.Count + _CollatzLookup[tmpNum];
-                        calculated = true;
-                    }
-
-                } while (!calculated);
-
-                if (cycleLength == 0)
-                {
-                    cycleLength = sequence.Count;
+                    cycleLengths.Add(ComputeCycleLength(num));
                 }
-
-                if (!_CollatzLookup.ContainsKey(num))
+                else
                 {
-                    _CollatzLookup.Add(num, cycleLength);
+                    Console.WriteLine("Number is invalid");
+                    break;
                 }
-
-                cycleLength = _CollatzLookup[num];
-                Console.WriteLine("DONE. Cycle length: {0}", cycleLength);
-            }
-            else
-            {
-                Console.WriteLine("Number is invalid");
             }
 
+            Console.WriteLine("Max cycle length between {0} and {1} is: {2}", i, j, cycleLengths.Max());
             Console.ReadLine();
+        }
+
+        static int ComputeCycleLength(int num)
+        {
+            int tmpNum = num;
+            var sequence = new List<int>();
+            int cycleLength = 0;
+            bool calculated = false;
+
+            sequence.Add(tmpNum);
+
+            do
+            {
+                tmpNum = f(tmpNum);
+                sequence.Add(tmpNum);
+                if (tmpNum == 1)
+                {
+                    calculated = true;
+                }
+                else if (_CollatzLookup.ContainsKey(tmpNum))
+                {
+                    cycleLength = sequence.Count + _CollatzLookup[tmpNum] - 1; // The -1 is because we're counting the found lookup twice.
+                    calculated = true;
+                }
+
+            } while (!calculated);
+
+            if (cycleLength == 0)
+            {
+                cycleLength = sequence.Count;
+            }
+
+            if (!_CollatzLookup.ContainsKey(num))
+            {
+                _CollatzLookup.Add(num, cycleLength);
+            }
+
+            return cycleLength;
         }
 
         static bool CheckNumber(int num)
@@ -96,7 +108,8 @@ namespace CollatzConjecture
             _CollatzLookup = new Dictionary<int, int>();
 
             // seed with powers of 2.
-            // 2^p has p steps to 1.
+            // 2^p has a cycle length of p+1.
+            // 16 = 2^4: {16, 8, 4, 2, 1}
 
             for (int i = 1; i <= MAX_EXPONENT; i++)
             {
